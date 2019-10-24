205ce75c5017ab524fc7c2007d7ee06ec44b613f
Allisson Dedigo <allisson.dedigo@fiergs.org.br>
2019-10-23 22:39:15 -0300

    Initial page config and get list JSON-SERVER

--- src/components/listaChurras.js
+++ src/components/listaChurras.js
@@ -1,13 +1,88 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  TouchableWithoutFeedback,
+  FlatList,
+  Text,
+  View,
+} from 'react-native';
+import api from '../services/api';
 
-const ListaChurrasScreen = ({navigation}) => {
-    console.log(navigation);
+const Item = ({title, selecionado, onPress, onLongPress}) => {
+  console.log(selecionado);
+  return (
+    <TouchableWithoutFeedback onPress={onPress} onLongPress={onLongPress}>
+      <View style={styles.itemContainer}>
+        <Text
+          style={selecionado ? styles.itemTextSelecionado : styles.itemText}>
+          {title}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default class MainScreen extends Component {
+  static navigationOptions = {
+    title: 'Lista de Churras',
+  };
+
+  state = {
+    churras: [],
+  };
+
+  componentWillMount() {
+    this.getListaChurras();
+  }
+
+  getListaChurras = async () => {
+    const response = await api.get('/churras');
+    const lista = response.data;
+    this.setState({churras: lista});
+  };
+
+  render() {
     return (
-        <View>
-            <Text>Hello Churras</Text>
-        </View>
-    )
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.churras}
+          extraData={this.state}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) => (
+            <Item
+              title={item.tarefa}
+              selecionado={item.selecionado}
+              onPress={() => this.selecionar(item.id)}
+              onLongPress={() => this.mostrarTarefa(item.id)}
+            />
+          )}
+        />
+      </View>
+    );
+  }
 }
 
-export default ListaChurrasScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  itemContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderBottomColor: '#DDD',
+    padding: 20,
+  },
+  itemText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  itemTextSelecionado: {
+    fontSize: 20,
+    textDecorationLine: 'line-through',
+    color: '#888',
+  },
+});
+
+//export default ListaChurrasScreen;
--- src/components/main.js
+++ src/components/main.js
@@ -1,28 +1,57 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 import api from '../services/api';
 
 export default class MainScreen extends Component {
-  
-  goToListChurras = () =>{
+  static navigationOptions = {
+    title: 'Que tal um Churras?',
+  };
+
+  goToListChurras = () => {
     this.props.navigation.navigate('ListaChurras');
-  }
-  
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Que Tal Um Churras?</Text>
-        <Button title="Lista de Churras" onPress={()=>this.goToListChurras()}></Button>
+          <TouchableOpacity style={[styles.op, styles.colorOp1]} onPress={()=>this.goToListChurras()}>
+            <Text style={styles.textOp}> Novo Churras? </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.op, styles.colorOp2]} onPress={()=>this.goToListChurras()}>
+            <Text style={styles.textOp}> Lista de Churras </Text>
+          </TouchableOpacity>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: "center",
+    //alignItems:"center"
+  },
 
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems:"center"   
-    }
+  op: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '50%',
+  },
+
+  colorOp1: {
+    backgroundColor: '#333333',
+  },
+
+  colorOp2: {
+    backgroundColor: '#c3c3c3',
+  },
+
+  textOp: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
 });
+//<Text>Que Tal Um Churras?</Text>
+//<Button title="Lista de Churras" onPress={()=>this.goToListChurras()}></Button>
